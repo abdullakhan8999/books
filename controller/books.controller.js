6ba838a31c0cea0d680da148ada8ccf1671940e0
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2022-12-01 17:06:04 +0530

    Filter Assignment

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -1,14 +1,75 @@
 const db = require("./../model/index");
+const Op = db.sequelize.Op;
 
 exports.get_All_books = async (req, res, next) => {
-  try {
-    const books = await db.books.findAll();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(`All books details:\n${JSON.stringify(books, null, 2)}`);
-    res.end();
-  } catch (error) {
-    next(error);
+  // Supporting the query param
+  const min_publication = req.query.min_publication;
+  const max_publication = req.query.max_publication;
+  const min_price = req.query.min_price;
+  const max_price = req.query.max_price;
+  let promise = null;
+  if (min_publication) {
+    promise = db.books.findAll({
+      where: {
+        publication: {
+          [Op.gte]: min_publication,
+        },
+      },
+    });
+  } else if (max_publication) {
+    promise = db.books.findAll({
+      where: {
+        publication: {
+          [Op.lte]: max_publication,
+        },
+      },
+    });
+  } else if (min_publication && max_publication) {
+    promise = db.books.findAll({
+      where: {
+        publication: {
+          [Op.gte]: min_publication,
+          [Op.lte]: max_publication,
+        },
+      },
+    });
+  } else if (min_price && max_price) {
+    promise = db.books.findAll({
+      where: {
+        price: {
+          [Op.gte]: min_price,
+          [Op.lte]: max_price,
+        },
+      },
+    });
+  } else if (min_price) {
+    promise = db.books.findAll({
+      where: {
+        price: {
+          [Op.gte]: min_price,
+        },
+      },
+    });
+  } else if (max_price) {
+    promise = db.books.findAll({
+      where: {
+        price: {
+          [Op.lte]: max_price,
+        },
+      },
+    });
+  } else {
+    promise = db.books.findAll();
   }
+  promise
+    .then((books) => {
+      res.status(200).send(`Books details:\n${JSON.stringify(books, null, 2)}`);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Some Internal error while fetching all the Books",
+      });
+    });
 };
 
 exports.get_Book = async (req, res, next) => {
@@ -25,14 +86,24 @@ exports.get_Book = async (req, res, next) => {
 };
 
 exports.post_Book = async (req, res, next) => {
-  try {
-    let book = req.body;
-    await db.books.create(book);
-    res.status(201).send("New book added");
-    res.end();
-  } catch (err) {
-    next(err);
-  }
+  const book = req.body;
+  await db.books
+    .create(book)
+    .then((book) => {
+      console.log(`Book name: [ ${book.title}] got inserted in DB`);
+      res.status(201).send(
+        `Book name: ${book.title} got inserted in DB 
+          ${JSON.stringify(book, null, 2)}`
+      );
+    })
+    .catch((err) => {
+      console.log(
+        `Issue in inserting Book name: [ ${book.title}]. Error message : ${err.message}`
+      );
+      res.status(500).send({
+        message: "Some Internal error while storing the book!",
+      });
+    });
 };
 
 exports.put_Book = async (req, res, next) => {
